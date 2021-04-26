5dd3391c6820c2866c49e01d0d4caa8ab354f6ca
Bartek Mucha <bartekmucha43@gmail.com>
2021-04-26 20:15:10 +0200

    Add Redux - new columns and cards

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,19 +2,24 @@ import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card';
-// import {settings} from '../../data/dataStore';
-// import Creator from '../Creator/Creator';
+import Creator from '../Creator/Creator';
 import Icon from '../Column/Icon';
+import { settings } from '../../data/dataStore';
 
 class Column extends React.Component {
     static propTypes = {
       icon: PropTypes.string.isRequired,
       title: PropTypes.string.isRequired,
       cards: PropTypes.array,
+      addCard: PropTypes.func,
+    }
+
+    static defaultProps = {
+      icon: settings.defaultColumnIcon,
     }
 
     render() {
-      const {title, icon, cards} = this.props;
+      const {title, icon, cards, addCard} = this.props;
       return (
         <section className={styles.component}>
           <h3 className={styles.title}>
@@ -22,11 +27,9 @@ class Column extends React.Component {
               <Icon name={icon}/>
             </span> {title}
           </h3>
-          {/*
           <div className={styles.creator}>
-            <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
+            <Creator text={settings.cardCreatorText} action={addCard}/>
           </div>
-          */}
           <div className={styles.cards}>
             {cards.map(cardData => (
               <Card key={cardData.id} {...cardData} />
