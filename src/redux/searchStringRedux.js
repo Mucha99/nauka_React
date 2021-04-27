f5a4040069fb5ca8cec4fc44ed4a18328d99a046
Bartek Mucha <bartekmucha43@gmail.com>
2021-04-27 22:14:27 +0200

    Add Redux - card filtering

---
+++ src/redux/searchStringRedux.js
@@ -0,0 +1,29 @@
+// selectors
+export const getSearchString = ({searchString}) => searchString;
+
+export const countVisibleCards = ({cards, searchString}) => cards.filter(
+  card => new RegExp(searchString, 'i').test(card.title)
+).length;
+
+export const countAllCards = ({cards}) => cards.length;
+
+// action name creator
+const reducerName = 'search';
+const createActionName = name => `app/${reducerName}/${name}`;
+
+// actions types
+export const ADD_SEARCH = createActionName('ADD_SEARCH');
+
+// action creators
+export const createAction_changeSearchString = payload => ({payload, type: ADD_SEARCH });
+
+// reducer
+export default function reducer(statePart = '', action = {}) {
+  switch (action.type) {
+    case ADD_SEARCH:
+      return action.payload;
+    default:
+      return statePart;
+  }
+}
+
