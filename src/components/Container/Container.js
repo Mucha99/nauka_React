686d8274e8a0a05d27b7a1d4a25c996bb044193c
Bartek Mucha <bartekmucha43@gmail.com>
2021-04-29 20:43:34 +0200

    Add Spa - Container.js/scss

---
+++ src/components/Container/Container.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Container.scss';
+import PropTypes from 'prop-types';
+
+const Container = ({children}) => (
+  <div className={styles.container}>
+    {children}
+  </div>
+);
+
+Container.propTypes = {
+  children: PropTypes.node, 
+};
+
+export default Container;
